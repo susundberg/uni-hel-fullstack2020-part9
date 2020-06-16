d28f1ffec661e7929b2ac559359fb161e07e5e6d
Pauli Salmenrinne <susundberg@gmail.com>
2020-06-16 14:20:13 +0300

    Exercise 9.15

--- ex_14/src/index.tsx
+++ ex_14/src/index.tsx
@@ -3,34 +3,94 @@ import ReactDOM from "react-dom";
 
 const courseName = "Half Stack application development";
 
-type CourseBase = {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
-const courseList: Array<CourseBase> = [
+interface CoursePartBaseWD extends CoursePartBase { // Ya, its not good name but its bit artificial anyway, eh?
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartBaseWD {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBaseWD {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+
+interface CoursePartFour extends CoursePartBaseWD {
+  name: "Really deep end";
+  secretLink: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+const courseList: CoursePart[] = [
   {
     name: "Fundamentals",
-    exerciseCount: 10
+    exerciseCount: 10,
+    description: "This is an awesome course part"
   },
   {
     name: "Using props to pass data",
-    exerciseCount: 7
+    exerciseCount: 7,
+    groupProjectCount: 3
   },
   {
     name: "Deeper type usage",
-    exerciseCount: 14
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+  },
+  {
+    name: "Really deep end",
+    exerciseCount: 7,
+    secretLink: "http://www.dontell.me",
+    description: "You really dont know"
   }
 ];
 
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+
+
 const Header: React.FC<{ name: string }> = ({ name }) => (<h1> {name} </h1>)
-const Content: React.FC<{ courses: Array<CourseBase> }> = ({ courses }) => (
+const Content: React.FC<{ courses: Array<CoursePart> }> = ({ courses }) => (
   <div>
-    {courses.map(x => (<p key={x.name}> {x.name} - {x.exerciseCount} </p>))}
+    {courses.map(x => {
+      switch (x.name) {
+        case "Fundamentals":
+          return (<p> F: {x.name} - {x.exerciseCount} - Desc: {x.description} </p>)
+        case "Using props to pass data":
+          return (<p> U: {x.name} - {x.exerciseCount} - Group: {x.groupProjectCount} </p>)
+        case "Deeper type usage":
+          return (<p> D: {x.name} - {x.exerciseCount} - Desc: {x.description} - Link: {x.exerciseSubmissionLink} </p>)
+          case "Really deep end":
+            return (<p> D: {x.name} - {x.exerciseCount} - Desc: {x.description}  - SECRET Link: {x.secretLink} </p>)
+          default:
+          assertNever(x);
+          return <p></p>
+      }
+
+    })}
   </div>
 )
 
-const Total: React.FC<{ courses: Array<CourseBase> }> = ({ courses }) => (
+const Total: React.FC<{ courses: Array<CoursePart> }> = ({ courses }) => (
   <p>
     Number of exercises{" "}
     {courses.reduce((carry, part) => carry + part.exerciseCount, 0)}
