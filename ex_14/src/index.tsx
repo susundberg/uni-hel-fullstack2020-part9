4f203749071b05f7c50d90a814b683003ebd6237
Pauli Salmenrinne <susundberg@gmail.com>
2020-06-16 14:03:20 +0300

    Exercise 9.14.

--- ex_14/src/index.tsx
+++ ex_14/src/index.tsx
@@ -1,23 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App: React.FC = () => {
-  const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
+const courseName = "Half Stack application development";
 
+type CourseBase = {
+  name: string;
+  exerciseCount: number;
+}
+
+const courseList: Array<CourseBase> = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14
+  }
+];
+
+const Header: React.FC<{ name: string }> = ({ name }) => (<h1> {name} </h1>)
+const Content: React.FC<{ courses: Array<CourseBase> }> = ({ courses }) => (
+  <div>
+    {courses.map(x => (<p key={x.name}> {x.name} - {x.exerciseCount} </p>))}
+  </div>
+)
+
+const Total: React.FC<{ courses: Array<CourseBase> }> = ({ courses }) => (
+  <p>
+    Number of exercises{" "}
+    {courses.reduce((carry, part) => carry + part.exerciseCount, 0)}
+  </p>
+)
+
+const App: React.FC = () => {
+
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courses={courseList} />
+      <Total courses={courseList} />
+    </div>
+  )
+
+  /*
   return (
     <div>
       <h1>{courseName}</h1>
@@ -35,7 +66,7 @@ const App: React.FC = () => {
         {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
       </p>
     </div>
-  );
+  );*/
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
