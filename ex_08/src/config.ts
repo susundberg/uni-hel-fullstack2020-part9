3c376e20a3ac9dce94d2620f68d3cd078a260d80
Pauli Salmenrinne <susundberg@gmail.com>
2020-06-16 09:52:23 +0300

    Exercises 9.8.-9.9.

---
+++ ex_08/src/config.ts
@@ -0,0 +1,13 @@
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT  = Number( process.env.PORT );
+
+
+
+
+export default  {
+    PORT,
+};
--- ex_08/src/index.ts
+++ ex_08/src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import cors from 'cors';
+import config from './config';
+
+
+
 const app = express();
+app.use(cors);
 app.use(express.json());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => { 
+
+app.get('/api/ping', (_req, res) => { 
   console.log('someone pinged here');
-  res.send('pong');
+  res.json({pong:true});
 });
   
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
+
